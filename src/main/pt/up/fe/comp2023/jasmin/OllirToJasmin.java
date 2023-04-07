c73a7e86b9812899584019c14f4bfd171668ca1d
inescardos <ines.caardoso@gmail.com>
2023-04-07 18:30:20 +0100

    Creating fields and methods declaration :)

--- src/main/pt/up/fe/comp2023/jasmin/OllirToJasmin.java
+++ src/main/pt/up/fe/comp2023/jasmin/OllirToJasmin.java
@@ -1,7 +1,9 @@
 package pt.up.fe.comp2023.jasmin;
 
 import org.specs.comp.ollir.ClassUnit;
+import org.specs.comp.ollir.Element;
 import org.specs.comp.ollir.Field;
+import org.specs.comp.ollir.Method;
 
 import java.util.ArrayList;
 import java.util.StringJoiner;
@@ -22,6 +24,7 @@ public class OllirToJasmin {
 
         code += createHeader();
         code += createFields();
+        code += createMethods();
 
         return code;
     }
@@ -39,11 +42,10 @@ public class OllirToJasmin {
 
     public String createFields() {
         String code = "";
-        ArrayList<Field> fields = classUnit.getFields();
 
-        for (Field field : fields) {
+        for (Field field : classUnit.getFields())
             code += createOneField(field) +  '\n';
-        }
+
 
         return code + "\n";
     }
@@ -53,16 +55,70 @@ public class OllirToJasmin {
         String code = ".field ";
 
         String fieldPrivacy = field.getFieldAccessModifier().name();
-        String fieldAccessModifiers = accessModifiers(fieldPrivacy, field.isFinalField(), field.isStaticField());
-        String fieldName = field.getFieldName();
-        String fieldType = jasminUtils.getType(field.getFieldType().getTypeOfElement());
-        code += fieldPrivacy + fieldAccessModifiers + fieldName + fieldType;
+        String fieldAccessModifiers = createAccessModifiers(fieldPrivacy, field.isFinalField(), field.isStaticField());
+        String fieldName = field.getFieldName() + " ";
+        String fieldType = jasminUtils.getType(field.getFieldType().getTypeOfElement()) + " ";
+        code += fieldAccessModifiers +  fieldName + fieldType;
         code += field.isInitialized() ? "=" + field.getInitialValue() : "";
 
         return code;
     }
 
-    public String accessModifiers(String privacy, Boolean isFinal, Boolean isStatic)
+    public String createMethods() {
+
+        String code = "";
+
+        for (Method method : classUnit.getMethods()) {
+            code += createOneMethod(method);
+        }
+
+        return code;
+    }
+
+    public String createOneMethod(Method method){
+        String code = "";
+
+        if(method.isConstructMethod())
+            createConstructMethod(method);
+        else{
+            code += createMethodDeclaration(method);
+            code += createMethodBody(method) + "\n";
+            code += ".end method\n";
+        }
+
+        return code;
+    }
+
+    public String createConstructMethod(Method method){
+        String code = "";
+        return code;
+    }
+
+    public String createMethodDeclaration(Method method){
+
+        String code = ".method ";
+
+        String methodPrivacy = method.getMethodAccessModifier().name();
+        String methodAcessModifiers = createAccessModifiers(methodPrivacy, method.isFinalMethod(), method.isStaticMethod());
+        String methodName = method.getMethodName();
+        code += methodAcessModifiers + methodName + '(';
+
+        for(Element param : method.getParams())
+            code += param.getType().getTypeOfElement();
+
+        String methodReturnType = jasminUtils.getType(method.getReturnType().getTypeOfElement());
+        code += ')' + methodReturnType;
+
+        return code;
+    }
+
+    public String createMethodBody(Method method){
+        String code = "";
+
+        return code;
+    }
+
+    public String createAccessModifiers(String privacy, Boolean isFinal, Boolean isStatic)
     {
         String code = "";
 
