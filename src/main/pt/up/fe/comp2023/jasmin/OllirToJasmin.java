5b4bb243d27fa2d7ca8992c564763dc0177406bd
inescardos <ines.caardoso@gmail.com>
2023-05-23 21:51:26 +0100

    limit stack?

--- src/main/pt/up/fe/comp2023/jasmin/OllirToJasmin.java
+++ src/main/pt/up/fe/comp2023/jasmin/OllirToJasmin.java
@@ -2,7 +2,10 @@ package pt.up.fe.comp2023.jasmin;
 
 import org.specs.comp.ollir.*;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class OllirToJasmin {
 
@@ -118,7 +121,9 @@ public class OllirToJasmin {
     private String getMethodLimits(Method method) {
         String code = "";
 
-        Set<Integer> registers = new TreeSet<>();
+        Set<Integer> registers = new HashSet<>();
+        registers.add(0);
+
         for(Map.Entry<String, Descriptor> var: method.getVarTable().entrySet()){
            registers.add(var.getValue().getVirtualReg());
         }
