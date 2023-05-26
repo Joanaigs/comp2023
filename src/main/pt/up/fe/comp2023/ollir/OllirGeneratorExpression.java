d86f9b5ea578223966689105467111ebbd230c46
marianaosiecka <mari.osiecka.carvalho@gmail.com>
2023-05-26 17:45:37 +0100

    fixing an error

--- src/main/pt/up/fe/comp2023/ollir/OllirGeneratorExpression.java
+++ src/main/pt/up/fe/comp2023/ollir/OllirGeneratorExpression.java
@@ -80,6 +80,11 @@ public class OllirGeneratorExpression extends AJmmVisitor<String, String> {
     private String visitBinaryOp(JmmNode jmmNode, String s) {
         String left = visit(jmmNode.getJmmChild(0), s);
         String right = visit(jmmNode.getJmmChild(1), s);
+        if(jmmNode.getJmmParent().getKind().equals("Assignment")){
+            String type = Utils.typeOllir(jmmNode);
+            String typeOp = Utils.typeOllir(jmmNode.getJmmChild(0));
+            return String.format("%s %s.%s %s", left, jmmNode.get("op"), typeOp, right);
+        }
         String newTempVar = "t" + this.numTemVars++;
         String type = Utils.typeOllir(jmmNode);
         String typeOp = Utils.typeOllir(jmmNode.getJmmChild(0));
