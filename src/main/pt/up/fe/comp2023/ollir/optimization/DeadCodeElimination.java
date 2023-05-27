1f4cabe1e1122a266fe42f8cb1726bdd27b1068e
Joana <up202006279@edu.fe.up.pt>
2023-05-27 14:29:17 +0100

    cleaned up code

--- src/main/pt/up/fe/comp2023/ollir/optimization/DeadCodeElimination.java
+++ src/main/pt/up/fe/comp2023/ollir/optimization/DeadCodeElimination.java
@@ -13,38 +13,33 @@ public class DeadCodeElimination extends AJmmVisitor<String,Boolean> {
     }
 
     private Boolean visitWhileStmt(JmmNode jmmNode, String s) {
-
-        // while (false) {}
         if (jmmNode.getJmmChild(0).getKind().equals("Boolean")) {
             if (jmmNode.getJmmChild(0).get("bool").equals("false")) {
                 jmmNode.delete();
                 return true;
             }
-
         }
         return visit(jmmNode.getJmmChild(1));
     }
 
     private Boolean visitIfStmt(JmmNode jmmNode, String s) {
         boolean hasChanges = false;
-        JmmNode condition = jmmNode.getJmmChild(0);
-        JmmNode ifStatement = jmmNode.getJmmChild(1);
-        JmmNode elseStatement = jmmNode.getJmmChild(2);
-
-
-        if (condition.getKind().equals("Boolean")) {
-            String value = condition.get("bool");
-            if (value.equals("true")) {
-                hasChanges = deleateIf(jmmNode, ifStatement);
-                hasChanges = hasChanges||visit(ifStatement);
-            } else if (value.equals("false")) {
-                hasChanges = deleateIf(jmmNode, elseStatement);
-                hasChanges = hasChanges||visit(elseStatement);
+        JmmNode cond = jmmNode.getJmmChild(0);
+        JmmNode ifNode = jmmNode.getJmmChild(1);
+        JmmNode elseNode = jmmNode.getJmmChild(2);
+        if (cond.getKind().equals("Boolean")) {
+            String bool = cond.get("bool");
+            if (bool.equals("true")) {
+                hasChanges = deleateIf(jmmNode, ifNode);
+                hasChanges = hasChanges||visit(ifNode);
+            } else if (bool.equals("false")) {
+                hasChanges = deleateIf(jmmNode, elseNode);
+                hasChanges = hasChanges||visit(elseNode);
             }
         }
         else{
-            hasChanges= visit(ifStatement);
-            hasChanges= hasChanges||visit(elseStatement);
+            hasChanges= visit(ifNode);
+            hasChanges= hasChanges||visit(elseNode);
         }
         return hasChanges;
     }
@@ -52,15 +47,13 @@ public class DeadCodeElimination extends AJmmVisitor<String,Boolean> {
     private boolean deleateIf(JmmNode jmmNode, JmmNode tochange) {
         if (jmmNode != null) {
             JmmNode newJmmNode = jmmNode.getJmmParent();
-            int start_index = jmmNode.getIndexOfSelf();
-
-            int i = 1;
+            int index = jmmNode.getIndexOfSelf();
+            index++;
             for (JmmNode child : tochange.getChildren()) {
-                newJmmNode.add(child, start_index + i);
-                i ++;
+                newJmmNode.add(child, index);
+                index++;
             }
             jmmNode.delete();
-
             return true;
         }
         return false;
